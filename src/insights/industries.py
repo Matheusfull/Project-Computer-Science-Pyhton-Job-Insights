caefbc229a5c85cfe24172d04277fb7e66a493a3
Matheus Felipe <fpmitmatheus@yahoo.com>
2023-02-28 16:08:22 -0500

    lint

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -26,6 +26,7 @@ def get_unique_industries(path: str) -> List[str]:
         )
     return industry
 
+
 # print(get_unique_industries('data/jobs.csv'))
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
