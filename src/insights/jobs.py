b04d3e78d1a645131284430604f3eface612ba8b
Matheus Felipe <fpmitmatheus@yahoo.com>
2023-03-01 11:48:27 -0500

    requisito 5

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -67,4 +67,11 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    filter_jobs = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            filter_jobs.append(job)
+    return filter_jobs
+
+# print(filter_by_job_type(read('data/jobs.csv'), "FULL_TIME"))
